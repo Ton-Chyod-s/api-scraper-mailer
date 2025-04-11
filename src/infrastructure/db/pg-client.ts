bd6c88f01cf8f61329b73dd50a3cdb79cfa654b0
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-04-11 19:16:51 -0400

    config: adicionado prisma migration

--- src/infrastructure/db/pg-client.ts
+++ src/infrastructure/db/pg-client.ts
@@ -9,9 +9,8 @@ export let pgClient = process.env.NODE_ENV === 'production'
     },
   }) 
 :  new Pool({
-    user: process.env.DB_USER,        
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME, 
-    password: process.env.DB_PASSWORD,     
-    port: Number(process.env.DB_PORT),                
+    connectionString: process.env.DATABASE_URL_TESTING,
+    ssl: {
+      rejectUnauthorized: false,
+    },              
 });
