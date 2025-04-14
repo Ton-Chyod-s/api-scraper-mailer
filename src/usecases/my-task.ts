26d3137bf41e401c094ab51c577596adb728f9ab
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-04-14 16:06:49 -0400

    config: configuração do email

--- src/usecases/my-task.ts
+++ src/usecases/my-task.ts
@@ -1,15 +1,24 @@
 import { NodemailerProvider } from "../infrastructure/providers/nodemailer-provider";
 import { SendEmailUseCase } from "./send-email-use-case";
+import { readFile } from 'fs/promises';
 
 export async function myTask() {
+    const ano = new Date().getFullYear().toString();
+
+    let htmlBase = await readFile("./src/static/main.html", "utf-8");
+    let header = await readFile("./src/static/emails/header.html", "utf-8");   
+
+    const conteudo = header;
+    const htmlFinal = htmlBase.replace("<main></main>", `<main>${conteudo}</main>`);
+
     const mailProvider = new NodemailerProvider();
     const sendMail = new SendEmailUseCase(mailProvider);
-
+ 
     await sendMail.execute({
         to: 'hix_x@hotmail.com',
-        subject: 'Test Email',
-        html: '<h1>Hello World</h1>'
+        subject: `Atualizações - UFMS, OTT, DOE, DIOGrande MS ${ano}`,
+        html: htmlFinal,
     }).then(() => {
         console.log("Email sent successfully!");
     });
-}
+}   
