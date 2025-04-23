c4012072ff9f30f0f286817281c1440489e98294
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-04-22 23:03:42 -0400

    doc: atualização da organização e nome das pastas

--- src/main/web/index.ts
+++ src/main/web/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { router } from '../../routes';	
+import { router } from '../../main/web/routes';	
 import '../../infrastructure/node-cron/scheduler'; 
 
 const server = express();
