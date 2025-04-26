0fc05833a3e6be8aa7d607aca81d618ef2be914b
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-04-26 07:50:54 -0400

    doc: att

--- src/infrastructure/node-cron/scheduler.ts
+++ src/infrastructure/node-cron/scheduler.ts
@@ -1,7 +1,7 @@
 import cron from 'node-cron';
 import { myTask } from '../../main/jobs/my-task';    
 
-cron.schedule('0 8 * * 1-5', () => {
+cron.schedule('0 8 * * 1-7', () => {
   console.log('Executando tarefa a cada minuto...');
   myTask();
 });
