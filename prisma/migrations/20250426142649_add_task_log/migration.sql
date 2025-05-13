96f6600be0193ba1555367d670d6a6e0c5b05c71
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-05-13 15:27:49 -0400

    feat: atualizar scripts de desenvolvimento e produção, adicionar suporte ao dotenv-safe e corrigir provider do banco de dados

--- prisma/migrations/20250426142649_add_task_log/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "task_log" (
-    "id" SERIAL NOT NULL,
-    "task_name" TEXT NOT NULL,
-    "executed_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "task_log_pkey" PRIMARY KEY ("id")
-);
