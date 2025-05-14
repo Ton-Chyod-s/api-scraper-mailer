989866513e19807896539b8a7ae02485e4a547fd
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-05-14 09:19:55 -0400

    feat: atualizar lógica de criação de usuários para suportar ambientes de desenvolvimento e produção

--- src/usecases/user/add-data.ts
+++ src/usecases/user/add-data.ts
@@ -1,14 +1,26 @@
 import { PrismaUserRepository } from "../../infrastructure/repositories/user/user-repository";
 import { CreateUser } from "./create-user";
 
+const NODE_ENV = process.env.NODE_ENV;
+
+if (!NODE_ENV) {
+    console.error("Variável de ambiente NODE_ENV não definida.");
+    process.exit(1); 
+}
+
 let userData: { nome: string; email: string }[] = [];
 
 try {
     userData = JSON.parse(process.env.USER_DATA_PROD || "[]");
 
+    if (NODE_ENV === "development") {
+        userData = JSON.parse(process.env.USER_DATA_DEV || "[]");
+    }
+    
     if (!Array.isArray(userData)) {
         throw new Error("USER_DATA deve ser um array.");
     }
+    
 } catch (error) {
     console.error("Erro ao carregar ou parsear USER_DATA:", error);
     process.exit(1); 
