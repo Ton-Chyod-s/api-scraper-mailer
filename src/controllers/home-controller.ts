cf1b94aef8f9b38981e4145089c7aa32922cd92c
Klayton COD.ias <65176457+Ton-Chyod-s@users.noreply.github.com>
2025-06-14 12:11:55 -0400

    Delete src/controllers directory

--- src/controllers/home-controller.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from "express";
-
-export class HomeController {
-    static async welcome ( req: Request, res: Response ) {
-        res.status(200).json({ message: "Hello World" });
-    }
-}
