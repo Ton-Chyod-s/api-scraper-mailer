82672cf52f9bd28d832dda9b3d6f062fbb2c41ab
Klayton COD.ias <klayton.dias@hotmail.com>
2025-06-13 22:54:22 -0400

    fix: remover arquivos de configuração desnecessários do PM2

--- pm2_startup/start_pm2.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "C:\Users\Klay\OneDrive\Documentos\GitHub Ton-Chyod-S\api-scraper-mailer\dist\main\web"
-pm2 start index.js --name api-scraper-mailer
-
-cd /d "C:\Users\Klay\.pm2"
-pm2 save
-pm2 resurrect
