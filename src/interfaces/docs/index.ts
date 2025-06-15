cb2b717b9778e17677018935a13482ee9deb32b9
Klayton COD.ias <klayton.dias@hotmail.com>
2025-06-15 08:42:53 -0400

    refactor: ajustar importações nos documentos e remover configurações desnecessárias do tsconfig

--- src/interfaces/docs/index.ts
+++ src/interfaces/docs/index.ts
@@ -1,8 +1,8 @@
-import { usuariosDoc } from './user/usuarios.doc.ts';
-import { exercitoDoc } from './exercito/exercito.doc.ts';
-import { diarioOficialEstadoDoc } from './diario-oficial/diario-oficial-estado.doc.ts';
-import { diarioOficialMunicipioDoc } from './diario-oficial/diario-oficial-municipio.doc.ts';
-import { authUserDoc } from './auth-user/auth-user.doc.ts';
+import { usuariosDoc } from './user/usuarios.doc';
+import { exercitoDoc } from './exercito/exercito.doc';
+import { diarioOficialEstadoDoc } from './diario-oficial/diario-oficial-estado.doc';
+import { diarioOficialMunicipioDoc } from './diario-oficial/diario-oficial-municipio.doc';
+import { authUserDoc } from './auth-user/auth-user.doc';
 
 export const swaggerPaths = {
   ...usuariosDoc,
