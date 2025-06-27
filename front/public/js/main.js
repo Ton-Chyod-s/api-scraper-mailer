eed3c1680981042efa3f54d1ec3c834f7e849412
Klayton COD.ias <klayton.dias@hotmail.com>
2025-06-27 15:22:31 -0400

    feat: remover arquivos de estilo e script não utilizados

--- front/public/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('JS carregado com sucesso!');
-  const botao = document.getElementById('meu-botao');
-  if (botao) {
-    botao.addEventListener('click', () => {
-      alert('Você clicou no botão!');
-    });
-  }
-});
