8db362c5799e51c40bb10eaa7f01b2256d460ccd
Klayton COD.ias <klayton.dias@hotmail.com>
2026-01-08 21:09:27 -0400

    feat: adicionar controladores para os diários oficiais dos municípios e estados

---
+++ src/interfaces/http/controllers/official-journals/official-journals-municipality.controller.ts
@@ -0,0 +1,6 @@
+import { OfficialJournalsMunicipalityUseCase } from "@usecases/official-journals/official-journals-municipality.use-case";
+
+export class OfficialJournalsMunicipalityController {
+  constructor(private readonly useCase: OfficialJournalsMunicipalityUseCase) {}
+
+}
---
+++ src/interfaces/http/controllers/official-journals/official-journals-state.controller.ts
@@ -0,0 +1,6 @@
+import { OfficialJournalsStateUseCase } from "@usecases/official-journals/official-journals-state.use-case";
+
+export class OfficialJournalsStateController {
+  constructor(private readonly useCase: OfficialJournalsStateUseCase) {}
+  
+}
